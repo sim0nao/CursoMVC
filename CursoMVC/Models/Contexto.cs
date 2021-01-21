b0d4c06f5ad6a1bf9af1b8473389c3ef7a5f4a68
Simone Campos <Simone.Campos@rckyinformatica.onmicrosoft.com>
2021-01-21 17:04:22 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CursoMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocalDB; Database = CursoMVC; Integrated Security = true");
+        }
+    }
+}
---
+++ Teste/CategoriasControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using CursoAPI.Controllers;
+using CursoMVC.Models;
+using Limilabs.Mail.PDI;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Teste
+{
+    public class CategoriasControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> mockSet;
+        private readonly Mock<Contexto> mockContext;
+        private readonly Categoria categoria;
+
+        public CategoriasControllerTest()
+        {
+            mockSet = new Mock<DbSet<Categoria>>();
+            mockContext = new Mock<Contexto>();
+            categoria = new Categoria { Id = 1, Descricao = "Teste de Categoria" };
+            mockContext.Setup(expression: m => m.Categorias.FindAsync(params keyValues: 1))
+                .ReturnsAsync(categoria);
+        }
+
+        [Fact]
+        public async Task GetCategoria()
+        {
+            var service = new CategoriasController(mockContext.Object);
+            var testCategoria = await service.GetCategoria(id:1);
+            mockSet.Verify(expression: m => m.FindAsync(params keyValues: 1),
+                Times.Once());
+            Assert.Equal(expected: categoria, actual: testCategoria);
+        }
+
+    }
+}
